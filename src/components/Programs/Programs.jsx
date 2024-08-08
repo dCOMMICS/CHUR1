c644c25567d6261dd76b385eb9c39454ecf54159
dCOMMICS <sanwurld@gmail.com>
2024-08-08 21:36:25 +0300

    adding images

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -3,6 +3,9 @@ import './Program.css'
 import program_1 from '../../assets/program-1.png'
 import program_2 from '../../assets/program-2.png'
 import program_3 from '../../assets/program-3.png'
+import program_icon_1 from '../../assets/program-icon-1.png'
+import program_icon_2 from '../../assets/program-icon-2.png'
+import program_icon_3 from '../../assets/program-icon-3.png'
 
 const Programs = () => {
   return (
@@ -10,15 +13,23 @@ const Programs = () => {
       <div className='program'>
         <img src ={program_1} alt = "" />
         <div className='caption'>
-            <img src = "" alt=""/>
+            <img src = {program_icon_1} alt=""/>
             <p>Graduate Degree</p>
         </div>
       </div>
         <div className='program'>
         <img src ={program_2} alt = "" />
+        <div className='caption'>
+            <img src = {program_icon_2} alt=""/>
+            <p>Masters Degree</p>
+        </div>
       </div>
         <div className='program'>
         <img src ={program_3} alt = "" />
+        <div className='caption'>
+            <img src = {program_icon_3} alt=""/>
+            <p>Bachelor Degree</p>
+        </div>
       </div>
     </div>
   )
