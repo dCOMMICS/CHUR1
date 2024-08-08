c0265715d108d3294e49d1d88fd8aa33e0fb3be2
dCOMMICS <sanwurld@gmail.com>
2024-08-08 06:04:26 +0300

    hero section progress 10%

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,19 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-      
+    <div className='hero container'>
+
+
+      <div className='hero-text'>
+        <h1>
+            {/* remember to change copy */}
+            We Ensure better eductation performance for a better world
+        </h1>
+        <p>
+            Our cutting-edge curriculum is designed to empower students with knowledge, skills, and experiences needed to improve in the dynamic field of education.
+        </p>
+        <button className='btn'>Explore more <img src='' alt='' /> </button>
+      </div>
     </div>
   )
 }
