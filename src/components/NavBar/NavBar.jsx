94e911ca8446120c7cb3172ede0fe548fef5524e
dCOMMICS <sanwurld@gmail.com>
2024-08-08 03:21:37 +0300

    initial commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './NavBar.css'
+// import logo
+
+const NavBar = () => {
+  return (
+    <nav>
+
+        {/* ADD IMAGES LATER WJEN URJ HAFLY DONE */}
+        {/* {logo} on the src path */}
+      <img src = "" alt = "CHT"/>
+      <ul> 
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li>Contact us</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
