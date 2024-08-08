55f47453098ea18fbaf722130adfb14d5c3e1f5e
dCOMMICS <sanwurld@gmail.com>
2024-08-08 11:13:51 +0300

    💯

---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Program.css'
+
+const Programs = () => {
+  return (
+    <div className='programs'>
+      <div className='program'>
+        <img src ="" alt = "" />
+      </div>
+    </div>
+  )
+}
+
+export default Programs
