ab162ee5f24868463aba035a472f281197286872
dCOMMICS <sanwurld@gmail.com>
2024-08-09 19:49:36 +0300

    initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div>
+      <div className = 'about'>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default About
