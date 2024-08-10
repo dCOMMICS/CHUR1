c17cc0d3b879e55afcaf74f09b6159bc73682f2b
dCOMMICS <sanwurld@gmail.com>
2024-08-11 01:20:42 +0300

    💪🏿

---
+++ src/components/Campus/Campus.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Campus.css'
+import gallery_1 from '../../assets/gallery-1.png'
+import gallery_2 from '../../assets/gallery-1.png'
+import gallery_3 from '../../assets/gallery-1.png'
+import gallery_4 from '../../assets/gallery-1.png'
+
+const Campus = () => {
+    return (
+        <div className='campus'>
+            <div className='gallery'>
+                <img className='' alt='' />
+            </div>
+        </div>
+    )
+}
+
+export default Campus
