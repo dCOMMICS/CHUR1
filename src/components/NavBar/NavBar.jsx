3c8ab5a11ba06739e43f244607e68866d47b0c1d
dCOMMICS <sanwurld@gmail.com>
2024-08-14 18:00:11 +0300

    initial commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './NavBar.css'
+import { Link } from 'react-scroll';
 // import logo from '../../assets/rhodess.png'
 // import logo
 
@@ -9,24 +10,38 @@ const NavBar = () => {
   const [sticky, setSticky] = useState(false);
 
   useEffect(() => {
-    window.addEventListener ('scroll', () => {
+    window.addEventListener('scroll', () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     })
   }, []);
 
   return (
-    <nav className={`container ${sticky ?  'dark-nav' : ''}`} >
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`} >
 
-        {/* Bild hinzufügen, wenn die Hälfte fertig ist*/}
-        {/* {logo} on the src path */}
-      <img src = "" alt = "CHT LOGO" className='logo'/>
-      <ul> 
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li> <button className='btn'>Contact us </button> </li>
+      {/* Bild hinzufügen, wenn die Hälfte fertig ist*/}
+      {/* {logo} on the src path */}
+      <img src="" alt="CHT LOGO" className='logo' />
+      <ul>
+        <li>
+          <Link to="hero" smooth={true} offset={0} duration={500}>Home</Link>
+        </li>
+        <li>
+          <Link to="program" smooth={true} offset={-260} duration={500}>Program</Link>
+          
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={-150} duration={500}>About us</Link>
+          
+        </li>
+        <li>
+          <Link to="campus" smooth={true} offset={-260} duration={500}>Campus</Link>
+          
+        </li>
+        <li>
+          <Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link>
+          
+        </li>
+        <li> <Link to="contact" smooth={true} offset={-260} duration={500} className='btn'>Contact us </Link> </li>
       </ul>
     </nav>
   )
